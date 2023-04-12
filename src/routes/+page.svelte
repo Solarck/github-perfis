d15f0b94fadc471334e5497203ee235e70048e94
Fernando Carlos <111884680+Solarck@users.noreply.github.com>
2023-04-12 20:00:52 -0300

    primeiro contato

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+     let texto = "Clique aqui para mudar o texto."
+
+     function trocaTexto(){
+        texto = "Novo texto após o clique!"
+     }
+  </script>
+  
+  <button class="title" on:click={trocaTexto}>{texto}</button>
+  
+  <style>
+    .title {
+      padding: 1rem 1.75rem;
+      font-family: 'Rubik', sans-serif;
+      font-size: 1.25rem;
+      font-weight: 300;
+      color: #fff;
+      background-color: #6930c3;
+      border: none;
+      border-radius: 0.5rem;
+    }
+  </style>
